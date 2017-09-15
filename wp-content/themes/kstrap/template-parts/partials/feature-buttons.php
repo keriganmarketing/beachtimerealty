ba66e3b186bd4b44bcacc3e192a66fb333afa262
Jack Kerigan <support@kerigan.com>
2017-09-15 11:15:54 -0400

    feature buttons

--- wp-content/themes/kstrap/template-parts/partials/feature-buttons.php
+++ wp-content/themes/kstrap/template-parts/partials/feature-buttons.php
@@ -6,3 +6,63 @@
  * @version 1.2
  */
 ?>
+<div class="container wide">
+	<div class="row">
+		<div class="col-6 col-lg-3">
+			<div class="button luxury-beachfront">
+				<div class="button-content">
+					<h2>Luxury Beachfront Homes</h2>
+					<div class="wave">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.24 9.33"><path class="cls-1" d="M0,5.92c2.52.23,5,.41,7.51.51s5,.12,7.41,0a53.45,53.45,0,0,0,7.23-.75,33.51,33.51,0,0,0,3.47-.8l3.59-1A99.71,99.71,0,0,1,44,.37a41,41,0,0,1,7.7-.31A31.52,31.52,0,0,1,59.24,1.4a63.54,63.54,0,0,0-7.48.41,61.87,61.87,0,0,0-7.25,1.31C39.74,4.27,35,5.87,30.2,7.27c-1.21.34-2.43.69-3.65,1A31.84,31.84,0,0,1,22.66,9a37,37,0,0,1-3.9.29c-1.3,0-2.59,0-3.88-.08a51,51,0,0,1-7.6-1.08C6,7.88,4.81,7.57,3.59,7.2A34.78,34.78,0,0,1,0,5.92Z"/></svg>
+					</div>
+				</div>
+				<a class="button-link" href="/properties/luxury-beachfront-homes/"></a>
+				<div class="button-overlay" ></div>
+			</div>
+		</div>
+		<div class="col-6 col-lg-3">
+			<div class="button condos">
+				<div class="button-content">
+					<h2 style="margin-top: 5%">Condos & Townhomes</h2>
+					<div class="wave">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.24 9.33"><path class="cls-1" d="M0,5.92c2.52.23,5,.41,7.51.51s5,.12,7.41,0a53.45,53.45,0,0,0,7.23-.75,33.51,33.51,0,0,0,3.47-.8l3.59-1A99.71,99.71,0,0,1,44,.37a41,41,0,0,1,7.7-.31A31.52,31.52,0,0,1,59.24,1.4a63.54,63.54,0,0,0-7.48.41,61.87,61.87,0,0,0-7.25,1.31C39.74,4.27,35,5.87,30.2,7.27c-1.21.34-2.43.69-3.65,1A31.84,31.84,0,0,1,22.66,9a37,37,0,0,1-3.9.29c-1.3,0-2.59,0-3.88-.08a51,51,0,0,1-7.6-1.08C6,7.88,4.81,7.57,3.59,7.2A34.78,34.78,0,0,1,0,5.92Z"/></svg>
+					</div>
+				</div>
+				<a class="button-link" href="/properties/luxury-beachfront-homes/"></a>
+				<div class="button-overlay" ></div>
+			</div>
+		</div>
+		<div class="col-6 col-lg-3">
+			<div class="button commercial">
+				<div class="button-content">
+					<h2 style="margin-top: 5%">Commercial Properties</h2>
+					<div class="wave">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.24 9.33"><path class="cls-1" d="M0,5.92c2.52.23,5,.41,7.51.51s5,.12,7.41,0a53.45,53.45,0,0,0,7.23-.75,33.51,33.51,0,0,0,3.47-.8l3.59-1A99.71,99.71,0,0,1,44,.37a41,41,0,0,1,7.7-.31A31.52,31.52,0,0,1,59.24,1.4a63.54,63.54,0,0,0-7.48.41,61.87,61.87,0,0,0-7.25,1.31C39.74,4.27,35,5.87,30.2,7.27c-1.21.34-2.43.69-3.65,1A31.84,31.84,0,0,1,22.66,9a37,37,0,0,1-3.9.29c-1.3,0-2.59,0-3.88-.08a51,51,0,0,1-7.6-1.08C6,7.88,4.81,7.57,3.59,7.2A34.78,34.78,0,0,1,0,5.92Z"/></svg>
+					</div>
+				</div>
+				<a class="button-link" href="/properties/luxury-beachfront-homes/"></a>
+				<div class="button-overlay" ></div>
+			</div>
+		</div>
+		<div class="col-6 col-lg-3">
+			<div class="button lots-land">
+				<div class="button-content">
+					<h2 style="margin-top: 10%">Lots & Land</h2>
+					<div class="wave">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59.24 9.33"><path class="cls-1" d="M0,5.92c2.52.23,5,.41,7.51.51s5,.12,7.41,0a53.45,53.45,0,0,0,7.23-.75,33.51,33.51,0,0,0,3.47-.8l3.59-1A99.71,99.71,0,0,1,44,.37a41,41,0,0,1,7.7-.31A31.52,31.52,0,0,1,59.24,1.4a63.54,63.54,0,0,0-7.48.41,61.87,61.87,0,0,0-7.25,1.31C39.74,4.27,35,5.87,30.2,7.27c-1.21.34-2.43.69-3.65,1A31.84,31.84,0,0,1,22.66,9a37,37,0,0,1-3.9.29c-1.3,0-2.59,0-3.88-.08a51,51,0,0,1-7.6-1.08C6,7.88,4.81,7.57,3.59,7.2A34.78,34.78,0,0,1,0,5.92Z"/></svg>
+					</div>
+				</div>
+				<a class="button-link" href="/properties/luxury-beachfront-homes/"></a>
+				<div class="button-overlay" ></div>
+			</div>
+		</div>
+	</div>
+	<div class="section-bottom">
+		<div class="section-button text-center">
+			<a class="btn btn-primary btn-outlined" href="/properties/" >Search All Property Types</a>
+		</div>
+		<div class="umbrella">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 69.75 54.75"><path class="cls-1" d="M33.11.49l-.2,0-.26,0-.24,0h0C15.78,3,2.88,12.62,0,23.55a10.75,10.75,0,0,1,10.78-2A24.23,24.23,0,0,1,24.16,3.37,24.24,24.24,0,0,0,10.78,21.56a19.45,19.45,0,0,1,14.2-2h0a26.13,26.13,0,0,1,9.35-3.06c.3,3.24,2.06,17.28,6.71,38.1l1.91.1a334.58,334.58,0,0,1-6.75-38.43,35.81,35.81,0,0,1,8.48.24,16.23,16.23,0,0,1,14.34-2C54.21,2,42.22.32,36.12.34,42.22.32,54.21,2,59,14.57c0,0,6.33-5.27,10.72-2.05C63.6,3.44,49.09-1.66,33.11.49Zm-.71.09-.14,0Zm-3.71,1A35.83,35.83,0,0,1,32.23.62h0a35.83,35.83,0,0,0-3.53.91,30.4,30.4,0,0,0-4.46,1.8A30.4,30.4,0,0,1,28.7,1.53Zm9.36,1.74A8.75,8.75,0,0,0,32.42.58h0A8.75,8.75,0,0,1,38.06,3.27Z"/></svg>
+		</div>
+	</div>
+</div>
