ea40e5753921c41c29b51faa9bbc5d2d3a8fda1d
Jack Kerigan <support@kerigan.com>
2017-11-03 10:41:43 -0400

    fixed link

--- wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
+++ wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
@@ -33,6 +33,6 @@ $now = time();
 
 <div class="section-bottom">
 	<div class="section-button text-center text-md-left">
-		<a class="btn btn-primary btn-outlined btn-reversed" href="#" >Follow us on Facebook</a>
+		<a class="btn btn-primary btn-outlined btn-reversed" href="https://www.facebook.com/beachtimerealtypcb/" >Follow us on Facebook</a>
 	</div>
 </div>
