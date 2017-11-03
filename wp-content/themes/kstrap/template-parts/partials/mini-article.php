63a8a6770d87e029fd41c6c2667293d2756ac22b
Jack Kerigan <support@kerigan.com>
2017-11-03 10:11:14 -0400

    facebook news page

---
+++ wp-content/themes/kstrap/template-parts/partials/mini-article.php
@@ -0,0 +1,34 @@
+<div class="card text-center">
+    <div class="article-image">
+        <?php if($result->type != 'video') { ?>
+            <div class="card-image embed-responsive embed-responsive-4by3">
+                <a href="<?php echo $result->link; ?>" target="_blank">
+                    <img src="<?php echo $photo_url; ?>" alt="<?php echo $result->caption; ?>" class="embed-responsive-item" >
+                </a>
+            </div>
+        <?php } else { ?>
+            <figure class="image video is-4by3">
+                <iframe
+                    src="<?php echo 'https://www.facebook.com/plugins/video.php?href='.$result->link ?>"
+                    style="border:none;overflow:hidden"
+                    scrolling="no"
+                    frameborder="0"
+                    allowTransparency="true"
+                    allowFullScreen="true"
+                    class="article-image"
+                    width="100%"
+                    height="460">
+
+                </iframe>
+            </figure>
+        <?php } ?>
+    </div>
+    <div class="card-block">
+        <p>posted <?php echo human_time_diff($now, strtotime($result->created_time)); ?> ago</p>
+        <p><?php echo $trimmed; ?></p>
+    </div>
+    <div class="card-footer">
+        <a class="article-footer-item" href="<?php echo $result->link; ?>" target="_blank" >Read more on Facebook</a>
+    </div>
+</div>
+
