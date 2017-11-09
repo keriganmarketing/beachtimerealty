c72ddf472007ff087489409544436bf4f76a7448
Daron Adkins <daron.adkins@gmail.com>
2017-11-09 09:32:43 -0600

    wip

--- wp-content/themes/kstrap/header.php
+++ wp-content/themes/kstrap/header.php
@@ -8,11 +8,15 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> >
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
-    <script>
-      gtag('config', 'UA-109417526-1');
-    </script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-109417526-1');
+</script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="-tilcek1viyq7BJTC8lYHoZ5bKbMsz6WF0cBhiIxS0U" />
