4d9673bb874a8947b78952741ae938037468490f
Daron Adkins <daron.adkins@gmail.com>
2017-11-09 09:24:57 -0600

    wip

--- wp-content/themes/kstrap/footer.php
+++ wp-content/themes/kstrap/footer.php
@@ -12,15 +12,6 @@
         $('.phone-mask').mask('(000)-000-0000');
     });
 </script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-109417526-1');
-</script>
 
 </body>
 </html>
--- wp-content/themes/kstrap/header.php
+++ wp-content/themes/kstrap/header.php
@@ -13,6 +13,15 @@
     <meta name="google-site-verification" content="-tilcek1viyq7BJTC8lYHoZ5bKbMsz6WF0cBhiIxS0U" />
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-109417526-1');
+    </script>
 </head>
 
 <body <?php body_class(); ?> >
