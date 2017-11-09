e177f5e5d870278692e0d61394408d69070d5287
Daron Adkins <daron.adkins@gmail.com>
2017-11-09 09:31:55 -0600

    wip

--- wp-content/themes/kstrap/header.php
+++ wp-content/themes/kstrap/header.php
@@ -8,20 +8,16 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> >
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
+    <script>
+      gtag('config', 'UA-109417526-1');
+    </script>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="-tilcek1viyq7BJTC8lYHoZ5bKbMsz6WF0cBhiIxS0U" />
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <?php wp_head(); ?>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109417526-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-109417526-1');
-    </script>
 </head>
 
 <body <?php body_class(); ?> >
