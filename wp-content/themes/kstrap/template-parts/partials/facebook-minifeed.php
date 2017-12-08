82ee4d3239a980414491cec6323ff0c369baee3e
Daron Adkins <daron.adkins@gmail.com>
2017-12-08 10:02:31 -0600

    facebook fallback photo

--- wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
+++ wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
@@ -10,14 +10,15 @@ $now     = time();
 <div class="facebook-feed">
 	<?php foreach ($results->posts as $result) {
         $message = $result->message ?? 'This just in...';
-		$trimmed = wp_trim_words( $message, $num_words = 22, '...' );
+        $trimmed = wp_trim_words( $message, $num_words = 22, '...' );
+        $photo_url = (! isset($result->full_picture) || $result->full_picture == '') ? 'https://beachtimerealty.com/wp-content/uploads/2017/09/Laketown-Wharf-real-estate-sales-team-Desiree-Gardner-Photography-Oct-2017-print-quality-30-e1510248636284-288x300.jpg' : $result->full_picture;
 		?>
 
         <div class="facebook-feed-item" id="<?php echo $result->id; ?>" >
 
             <div class="row">
                 <div class="col-4">
-                    <img src="<?php echo $result->full_picture; ?>" class="img-fluid" alt="<?php echo $result->caption ?? 'The photo'; ?>" >
+                    <img src="<?php echo $photo_url; ?>" class="img-fluid" alt="<?php echo $result->caption ?? 'The photo'; ?>" >
                 </div>
                 <div class="col-8">
                     <p class="time-posted">posted <?php echo human_time_diff($now,strtotime($result->created_time)); ?> ago</p>
