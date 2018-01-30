1b72c36e0633316085251b265b450bd5ef93cf87
Bryan Baird <bryan@kerigan.com>
2018-01-30 13:22:53 -0500

    temporarily remove siteby

--- wp-content/themes/kstrap/template-parts/partials/bot.php
+++ wp-content/themes/kstrap/template-parts/partials/bot.php
@@ -111,7 +111,7 @@ $offices = $agents->getOffices();
                     <p class="copyright">&copy;<?php echo date('Y'); ?> <?php echo get_bloginfo(); ?>. All Rights Reserved.</p>
                 </div>
                 <div class="col-md-3 my-auto justify-content-center justify-content-sm-end text-center text-sm-right">
-                    <p class="siteby"><svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="14" width="20" viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;" xml:space="preserve">
+                    <p class="siteby" style="display: none;"><svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="14" width="20" viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;" xml:space="preserve">
                                 <defs><style>.kma{fill:#b4be35;}</style></defs>
                             <path class="kma" fill="#b4e35" d="M6.4,0.1c0,0,0.1,0.3,0.2,0.9c1,3,3,5.6,5.7,7.2l-0.1,0.5c0,0-0.4-0.2-1-0.4C7.7,7,3.7,7,0.2,8.5L0.1,8.1
                                 c2.8-1.5,4.8-4.2,5.7-7.2C6,0.4,6.1,0.1,6.1,0.1H6.4L6.4,0.1z"/>
