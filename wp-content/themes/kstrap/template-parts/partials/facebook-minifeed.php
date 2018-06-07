6d0079e45e3fd92c04eb60c8523617cfc3de7eb8
Daron Adkins <daron.adkins@gmail.com>
2018-06-07 14:30:19 -0500

    fix facebook dbaggery

--- wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
+++ wp-content/themes/kstrap/template-parts/partials/facebook-minifeed.php
@@ -7,29 +7,31 @@ $results = $feed->fetch(2);
 $now     = time();
 
 ?>
-<div class="facebook-feed">
-	<?php foreach ($results->posts as $result) {
-        $message = $result->message ?? 'This just in...';
-        $trimmed = wp_trim_words( $message, $num_words = 22, '...' );
-        $photo_url = (! isset($result->full_picture) || $result->full_picture == '') ? 'https://beachtimerealty.com/wp-content/uploads/2017/09/Laketown-Wharf-real-estate-sales-team-Desiree-Gardner-Photography-Oct-2017-print-quality-30-e1510248636284-288x300.jpg' : $result->full_picture;
-		?>
+<?php if (! property_exists($results, 'error')) { ?>
+    <div class="facebook-feed">
+        <?php foreach ($results->posts as $result) {
+            $message = $result->message ?? 'This just in...';
+            $trimmed = wp_trim_words( $message, $num_words = 22, '...' );
+            $photo_url = (! isset($result->full_picture) || $result->full_picture == '') ? 'https://beachtimerealty.com/wp-content/uploads/2017/09/Laketown-Wharf-real-estate-sales-team-Desiree-Gardner-Photography-Oct-2017-print-quality-30-e1510248636284-288x300.jpg' : $result->full_picture;
+            ?>
 
-        <div class="facebook-feed-item" id="<?php echo $result->id; ?>" >
+            <div class="facebook-feed-item" id="<?php echo $result->id; ?>" >
 
-            <div class="row">
-                <div class="col-4">
-                    <img src="<?php echo $photo_url; ?>" class="img-fluid" alt="<?php echo $result->caption ?? 'The photo'; ?>" >
+                <div class="row">
+                    <div class="col-4">
+                        <img src="<?php echo $photo_url; ?>" class="img-fluid" alt="<?php echo $result->caption ?? 'The photo'; ?>" >
+                    </div>
+                    <div class="col-8">
+                        <p class="time-posted">posted <?php echo human_time_diff($now,strtotime($result->created_time)); ?> ago</p>
+                        <p style="margin:0;"><?php echo $trimmed; ?> <a target="_blank" href="<?php echo $result->permalink_url; ?>" >read more</a></p>
+                    </div>
                 </div>
-                <div class="col-8">
-                    <p class="time-posted">posted <?php echo human_time_diff($now,strtotime($result->created_time)); ?> ago</p>
-                    <p style="margin:0;"><?php echo $trimmed; ?> <a target="_blank" href="<?php echo $result->permalink_url; ?>" >read more</a></p>
-                </div>
+                <hr>
             </div>
-            <hr>
-        </div>
 
-	<?php } ?>
-</div>
+        <?php } ?>
+    </div>
+<?php } ?>
 
 <div class="section-bottom">
 	<div class="section-button text-center text-md-left">
