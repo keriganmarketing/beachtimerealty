22c11620421c3c9b7117266b3e7d4e75ce0f44ce
Bryan Baird <bryan@kerigan.com>
2018-12-03 22:32:29 +0000

    user content

--- wp-content/themes/kstrap/vendor/composer/autoload_static.php
+++ wp-content/themes/kstrap/vendor/composer/autoload_static.php
@@ -7,8 +7,9 @@ namespace Composer\Autoload;
 class ComposerStaticInite3b0963dd551d2fa8fb82fec2bf86750
 {
     public static $files = array (
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         'de85a44be454aa97188dad52ed888bed' => __DIR__ . '/..' . '/panique/laravel-sass/sass-compiler.php',
     );
